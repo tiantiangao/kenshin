88bb27489c36360c443a6347c90028ff608d7070
tiantian.gao <gao12581@sina.com>
2013-08-01 07:59:43 +0800

    修改单元测试

--- kenshin-log/src/main/java/com/gtt/kenshin/log/KenshinLogger.java
+++ kenshin-log/src/main/java/com/gtt/kenshin/log/KenshinLogger.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:kenshin-log/src/main/java/com/gtt/kenshin/log/KenshinLogger.java
 package com.gtt.kenshin.log;
 
 /**
@@ -50,56 +49,3 @@ public interface KenshinLogger {
 	void info(Object message);
 
 }
-=======
-package com.gtt.kenshin.log;
-
-/**
- * Log接口
- * 
- * @author tiantian.gao
- * 
- */
-public interface KenshinLogger {
-
-	/**
-	 * Debug level message
-	 */
-	void debug(Object message, Throwable t);
-
-	/**
-	 * Debug level message
-	 */
-	void debug(Object message);
-
-	/**
-	 * Warn level message
-	 */
-	void warn(Object message, Throwable t);
-
-	/**
-	 * Warn level message
-	 */
-	void warn(Object message);
-
-	/**
-	 * Error level message
-	 */
-	void error(Object message, Throwable t);
-
-	/**
-	 * Error level message
-	 */
-	void error(Object message);
-
-	/**
-	 * Info level message
-	 */
-	void info(Object message, Throwable t);
-
-	/**
-	 * Info level message
-	 */
-	void info(Object message);
-
-}
->>>>>>> dev:kenshin-log/src/main/java/com/gtt/kenshin/log/KenshinLogger.java
